c462653cc35c8ac7c0757a1ad81d36ce3cd0e455
LENOVO <LENOVO@Manoj>
2022-09-23 15:15:18 +0530

    commit 5 done

--- src/test/java/tyss/PracticeProjectSDET40/PracticeTestCase5.java
+++ src/test/java/tyss/PracticeProjectSDET40/PracticeTestCase5.java
@@ -3,7 +3,7 @@ package tyss.PracticeProjectSDET40;
 public class PracticeTestCase5 {
 	public void tc5() {
 		System.out.println(System.getProperty("B"));
-		System.out.println(System.getProperty("U"));
+		System.out.println(System.getProperty("UR"));
 		
 	}
 
