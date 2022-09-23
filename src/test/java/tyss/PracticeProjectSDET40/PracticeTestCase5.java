fd108bc98e73a89602e44ec7b0badb5d6cb0ef12
LENOVO <LENOVO@Manoj>
2022-09-23 15:24:26 +0530

    Commit manu done

--- src/test/java/tyss/PracticeProjectSDET40/PracticeTestCase5.java
+++ src/test/java/tyss/PracticeProjectSDET40/PracticeTestCase5.java
@@ -2,8 +2,8 @@ package tyss.PracticeProjectSDET40;
 
 public class PracticeTestCase5 {
 	public void tc5() {
-		System.out.println(System.getProperty("B"));
-		System.out.println(System.getProperty("UR"));
+		System.out.println(System.getProperty("Manoj"));
+		System.out.println(System.getProperty("Manu"));
 		
 	}
 
