b37a05f948f640a46a57ae41ea2345af04e38c22
LENOVO <LENOVO@Manoj>
2022-09-23 14:58:10 +0530

    new commit

---
+++ src/test/java/tyss/PracticeProjectSDET40/PracticeTestCase4.java
@@ -0,0 +1,13 @@
+package tyss.PracticeProjectSDET40;
+
+import org.testng.annotations.Test;
+
+public class PracticeTestCase4 {
+	@Test
+	public void tc4() {
+		System.out.println(System.getProperty("b"));
+		System.out.println(System.getProperty("u"));
+		
+	}
+
+}
