cb5049709a8fcb320e6e0e8837d3a5ffe847941a
LENOVO <LENOVO@Manoj>
2022-09-23 15:07:03 +0530

    Commit 5 done

---
+++ src/test/java/tyss/PracticeProjectSDET40/PracticeTestCase5.java
@@ -0,0 +1,10 @@
+package tyss.PracticeProjectSDET40;
+
+public class PracticeTestCase5 {
+	public void tc5() {
+		System.out.println(System.getProperty("b"));
+		System.out.println(System.getProperty("u"));
+		
+	}
+
+}
